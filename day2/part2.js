9547f1b167d92f3d032157221eb9b2243fc69a3b
claudi-tm <ccaducarlos@gmail.com>
2022-01-15 21:12:49 +0200

    done exe part 2

---
+++ day2/part2.js
@@ -0,0 +1,28 @@
+console.log(`The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another. `)
+
+// num 3
+console.log("10" === 10);
+// num 4
+if (parseFloat('9.8') == 10 ){
+    console.log(true);
+} else if (parseFloat("9.8") === 10) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+// num 5
+console.log("python".includes("on") && "jargon".includes("on"));
+// num 6
+console.log("I hope this course is not full of jargon".includes("jargon"))
+// num 7
+console.log(Math.floor(Math.random() * 101));
+// num 8
+console.log(Math.floor(Math.random() * (101 - 50 + 1)) + 50);
+// num 9
+console.log(Math.floor(Math.random() * 256));
+// num 11
+console.log(`1 1 1 1 1\n
+2 1 2 4 8\n
+3 1 3 9 27\n
+4 1 4 16 64\n
+5 1 5 25 125`);
