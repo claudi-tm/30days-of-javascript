a686d64ce3229e6ba6fc82e0a61bb139106cdc15
claudi-tm <ccaducarlos@gmail.com>
2022-01-16 05:53:00 +0200

    faild with the part 3 in some exercices

---
+++ day2/part3.js
@@ -0,0 +1,11 @@
+// num 1
+var sentence = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+console.log(`The word love was found ${sentence.match(/love/g).length} times`);  
+// num 2
+var sentence2 = 'You cannot end a sentence with because because because is a conjunction';
+console.log(`The word becaus was found ${sentence2.match(/because/g).length} times`);
+// num 3
+var sentece3 = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
+sentece3.split(" ");
+
+console.log("Income = " + income);
