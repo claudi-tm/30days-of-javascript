b85ba75a4f58abf6888313967f933e9f307d13a7
claudi-tm <ccaducarlos@gmail.com>
2022-01-20 14:09:32 +0200

    day 4 done

---
+++ day4/level 2/exercices.js
@@ -0,0 +1,15 @@
+// num 1
+console.log("EXE 1");
+let score = 100;
+if (score > 0 && score <= 49){
+    console.log("F");
+} else if (score >= 50 && score <= 59){
+    console.log("D");
+} else if (score >= 60 && score <= 69){
+    console.log("C");
+} else if (score >= 70 && score <= 79){
+    console.log("B");
+} else {
+    console.log("A");
+}
+
---
+++ day4/level 3/exe1.js
@@ -0,0 +1,50 @@
+let isleap = false;
+let month = "february".toLowerCase();
+let year = 2016;
+if (year % 4 == 0 && year % 100 != 0) {
+    isleap = true;
+} else if (year % 100 == 0 && year % 400 == 0) {
+    isleap = true;
+}
+
+switch (month) {
+    case "january":
+        console.log(month, " has 31 days");
+        break;
+    case "february":
+        isleap ? console.log(month, " has 29 days") : console.log(month, " has 28 days");
+        break;
+    case "march":
+        console.log(month, " has 31 days");
+        break;
+    case "april":
+        console.log(month, " has 30 days");
+        break;
+    case "may":
+        console.log(month, " has 31 days");
+        break;
+    case "june":
+        console.log(month, " has 30 days");
+        break;
+    case "july":
+        console.log(month, " has 31 days");
+        break;
+    case "august":
+        console.log(month, " has 31 days");
+        break;
+    case "september":
+        console.log(month, " has 30 days");
+        break;
+    case "october":
+        console.log(month, " has 31 days");
+        break;
+    case "november":
+        console.log(month, " has 30 days");
+        break;
+    case "december":
+        console.log(month, " has 31 days");
+        break;
+
+    default:
+        break;
+}
